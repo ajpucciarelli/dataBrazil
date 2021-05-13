08b8fc7c713efb6edcc04d2ba6be7843e8d654d4
Amilcar Pucciarelli <ajpucciarelli@gmail.com>
2021-05-12 22:49:52 -0300

    Retirada da senha

--- dao/engine.py
+++ dao/engine.py
@@ -4,10 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
 
 # sudo grep 'temporary password' /var/log/mysqld.log
-# root@localhost: JxlA&T1M_264
-# T00r@2021
 
-URL_MYSQL = 'mysql+pymysql://root:T00r@2021@localhost/legislativo'
+URL_MYSQL = 'mysql+pymysql://root:XXXXXX@localhost/legislativo'
 
 engine = create_engine(URL_MYSQL, echo=True)
 if not database_exists(engine.url):
